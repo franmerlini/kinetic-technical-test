1524907bf27c894740a1efa6208d44f29230adab
Francisco Merlini <merlinifrancisco@gmail.com>
2025-07-09 14:09:34 -0300

    chore: add commitlint, eslint and prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,12 @@
 import nx from '@nx/eslint-plugin';
+import eslint from '@eslint/js';
+import angular from 'angular-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
@@ -25,6 +31,16 @@ export default [
   ...nx.configs['flat/angular-template'],
   {
     files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      importPlugin.flatConfigs.recommended,
+      eslintPluginPrettierRecommended,
+      eslintConfigPrettier,
+    ],
+    processor: angular.processInlineTemplates,
     rules: {
       '@angular-eslint/directive-selector': [
         'error',
@@ -42,11 +58,26 @@ export default [
           style: 'kebab-case',
         },
       ],
+      '@angular-eslint/prefer-on-push-component-change-detection': 'error',
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      'no-console': 'error',
+      'no-else-return': 'error',
+      eqeqeq: 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'prefer-object-spread': 'error',
+      'prefer-template': 'error',
+      'import/no-unresolved': 'off',
+      'import/group-exports': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-dynamic-require': 'error',
+      'import/no-absolute-path': 'error',
+      'import/named': 'off',
     },
   },
   {
     files: ['**/*.html'],
-    // Override or add rules here
+    extends: [...angular.configs.templateRecommended, eslintPluginPrettierRecommended, eslintConfigPrettier],
     rules: {},
-  },
-];
+  }
+);
