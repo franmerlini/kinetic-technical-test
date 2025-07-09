9860a0308bb329ad91ce4ff2754ee2ec75701ca3
Francisco Merlini <merlinifrancisco@gmail.com>
2025-07-09 20:13:06 -0300

    test: add setup function

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+
 import { App } from './app';
 
 describe('App', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App],
-    }).compileComponents();
+  it('should create', () => {
+    const { component } = setup();
+    expect(component).toBeTruthy();
   });
 });
+
+function setup() {
+  TestBed.configureTestingModule({
+    imports: [App],
+  }).compileComponents();
+
+  const fixture = TestBed.createComponent(App);
+  const component = fixture.componentInstance;
+  fixture.detectChanges();
+  return { fixture, component };
+}
