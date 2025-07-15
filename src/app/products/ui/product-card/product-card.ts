40bfab758e4fb98f96f98d1602961ad86987e03f
Francisco Merlini <merlinifrancisco@gmail.com>
2025-07-15 09:41:28 -0300

    feat(products): add fallback image

--- src/app/products/ui/product-card/product-card.ts
+++ src/app/products/ui/product-card/product-card.ts
@@ -1,6 +1,5 @@
 import { CurrencyPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ChangeDetectionStrategy, Component, ElementRef, input, output, viewChild } from '@angular/core';
 
 import { Button } from 'primeng/button';
 import { Card } from 'primeng/card';
@@ -10,13 +9,24 @@ import { Product } from '@products/domain';
 
 @Component({
   selector: 'app-product-card',
-  imports: [Card, RouterLink, CurrencyPipe, Button, Tooltip],
+  imports: [Card, CurrencyPipe, Button, Tooltip],
   templateUrl: './product-card.html',
   styleUrl: './product-card.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductCard {
+  readonly img = viewChild<ElementRef<HTMLImageElement>>('img');
+
   readonly product = input.required<Product>();
 
+  readonly viewProduct = output<number>();
+  readonly updateProduct = output<number>();
   readonly deleteProduct = output<number>();
+
+  setPlaceholderImage(): void {
+    this.img()?.nativeElement.setAttribute(
+      'src',
+      'https://imgs.search.brave.com/4l4fU8BEQaCZG5y951ub0b6BzVolar0kLd8hvcSdNSo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdDMu/ZGVwb3NpdHBob3Rv/cy5jb20vMTc4Mjgy/NzgvMzMxNTAvdi80/NTAvZGVwb3NpdHBo/b3Rvc18zMzE1MDMy/NjItc3RvY2staWxs/dXN0cmF0aW9uLW5v/LWltYWdlLXZlY3Rv/ci1zeW1ib2wtbWlz/c2luZy5qcGc'
+    );
+  }
 }
--- src/app/products/ui/product-detail/product-detail.ts
+++ src/app/products/ui/product-detail/product-detail.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe, DatePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, input, output, viewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { Button } from 'primeng/button';
@@ -17,7 +17,16 @@ import { Product } from '@products/domain';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductDetail {
+  readonly img = viewChild<ElementRef<HTMLImageElement>>('img');
+
   readonly product = input.required<Product>();
 
   readonly deleteProduct = output<number>();
+
+  setPlaceholderImage(): void {
+    this.img()?.nativeElement.setAttribute(
+      'src',
+      'https://imgs.search.brave.com/4l4fU8BEQaCZG5y951ub0b6BzVolar0kLd8hvcSdNSo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdDMu/ZGVwb3NpdHBob3Rv/cy5jb20vMTc4Mjgy/NzgvMzMxNTAvdi80/NTAvZGVwb3NpdHBo/b3Rvc18zMzE1MDMy/NjItc3RvY2staWxs/dXN0cmF0aW9uLW5v/LWltYWdlLXZlY3Rv/ci1zeW1ib2wtbWlz/c2luZy5qcGc'
+    );
+  }
 }
